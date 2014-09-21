ebf4a8b3b8d86ae58462381988dc904dd768c499
Artur Minasjan <minasjan@gmail.com>
2014-09-22 00:11:17 +0300

    MessagesHistory

---
+++ SoSho/SOSMessageHistory.h
@@ -0,0 +1,12 @@
+//
+//  SOSMessageHistory.h
+//  SoSho
+//
+//  Created by Artur Minasjan on 14/09/14.
+//  Copyright (c) 2014 SoSho. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SOSMessageHistory : NSObject
+@end
---
+++ SoSho/SOSMessageHistory.m
@@ -0,0 +1,13 @@
+//
+//  SOSMessageHistory.m
+//  SoSho
+//
+//  Created by Artur Minasjan on 14/09/14.
+//  Copyright (c) 2014 SoSho. All rights reserved.
+//
+
+#import "SOSMessageHistory.h"
+
+@implementation SOSMessageHistory
+
+@end
