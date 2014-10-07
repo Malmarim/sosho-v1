b3541c3f0cc620419d752d674669d28c739b03f2
Artur Minasjan <minasjan@gmail.com>
2014-10-07 10:08:45 +0300

    Redoing cell for chat view

---
+++ SoSho/SOSMineMessageView.h
@@ -0,0 +1,13 @@
+//
+//  SOSMineMessageView.h
+//  SoSho
+//
+//  Created by Artur Minasjan on 06/10/14.
+//  Copyright (c) 2014 SoSho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SOSMineMessageView : UIView
+
+@end
